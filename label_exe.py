2bcccc400c0a919135f10602dc3f97a4d2e8e07b
zrf <www.ahdic@qq.com>
2020-09-16 22:16:20 +0800

    bug restore

--- label_exe.py
+++ label_exe.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter.filedialog import askdirectory
 from PIL import Image, ImageTk
 import os
+import glob
+import time
 import xlwt
 import xlrd
 import openpyxl
@@ -15,11 +17,11 @@ class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
-        self.image1_x = 0
-        self.image1_y = 50
-        self.image2_x = 0
-        self.image2_y = 400
-        self.fig_info, self.fig_name = self.readFile()
+        self.image_x = 20
+        self.image_y = 100
+        # self.image2_x = 0
+        # self.image2_y = 400
+        self.fig_name = self.readFile()
         self.num_fig = len(self.fig_name) / 2
         self.init_window()
         self.excel_dir = self.labelPath()
@@ -27,7 +29,7 @@ class Window(Frame):
         self.load_fig(self.idx)
 
     def init_window(self):
-        self.master.title("主导失稳模式样本标注程序v1.0")
+        self.master.title("主导失稳模式样本标注程序v2.0")
 
         self.pack(fill=BOTH, expand=1)
 
@@ -49,17 +51,20 @@ class Window(Frame):
         edit.add_command(label='Show  Text', command=self.showTxt)
         menu.add_cascade(label='编辑', menu=edit)
         ClickButton1 = Button(self, text='稳定', command=lambda: self.readExcel(0))
-        ClickButton1.place(x=750, y=200, width=100)
+        ClickButton1.place(x=840, y=200, width=100)
         ClickButton2 = Button(self, text='功角失稳', command=lambda: self.readExcel(1))
-        ClickButton2.place(x=750, y=400, width=100)
+        ClickButton2.place(x=840, y=400, width=100)
         ClickButton2 = Button(self, text='电压失稳', command=lambda: self.readExcel(2))
-        ClickButton2.place(x=750, y=600, width=100)
+        ClickButton2.place(x=840, y=600, width=100)
 
     def client_exit(self):
         exit()
 
     def showImg(self, image_dir, image_x, image_y):
-        load = Image.open(image_dir).resize((700, 350))
+        # load = Image.open(image_dir).resize((800, 600))
+        load = Image.open(image_dir)
+        width, height = load.size
+        load = load.resize((width//2, height//2))
         render = ImageTk.PhotoImage(load)
 
         img = Label(self, image=render)
@@ -67,8 +72,16 @@ class Window(Frame):
         img.place(x=image_x, y=image_y)
 
     def showTxt(self, idx):
-        text = Label(self, text=self.fig_name[idx])
-        text.place(x=0, y=0)
+        figure_name = self.fig_name[idx]
+        mode_id = figure_name[-5]
+        if mode_id == '1':
+            info = '自动判稳模式：稳定'
+        if mode_id == '2':
+            info = '自动判稳模式：功角失稳'
+        if mode_id == '3':
+            info = '自动判稳模式：电压失稳'
+        text = Label(self, text=info, font=('微软雅黑', 20, 'bold'))
+        text.place(x=200, y=20)
 
     def selectPath(self):
         path = askdirectory()
@@ -76,7 +89,8 @@ class Window(Frame):
         return path
 
     def samplePath(self):
-        self.fig_info, self.fig_name = self.readFile()
+
+        self.fig_name = self.readFile()
         self.idx = 0
         self.load_fig(self.idx)
 
@@ -90,6 +104,7 @@ class Window(Frame):
         file = xlrd.open_workbook(excel_dir)
         table = file.sheets()[0]  # 得到sheet页
         nrows = table.nrows  # 总行数
+        print(nrows)
         return excel_dir, nrows
 
     def readExcel(self, label):
@@ -100,32 +115,32 @@ class Window(Frame):
         wb.save(excel_dir)
         self.load_fig(self.idx)
 
+    def search_all_files_return_by_time_reversed(self, path, reverse=False):
+        return sorted(glob.glob(os.path.join(path, '*')),
+                      key=lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getmtime(x))),
+                      reverse=reverse)
+
     def readFile(self):
         path = self.selectPath()
-        fig_name = []
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                fig_name.append(root + '/' + file)
-        return files, fig_name
+        fig_name = self.search_all_files_return_by_time_reversed(path)
+        return fig_name
 
     def load_fig_dir(self, idx):
         fig_name = self.fig_name
-        image_dir1 = fig_name[2*idx]
-        image_dir2 = fig_name[2*idx + 1]
-        return image_dir1, image_dir2
+        image_dir1 = fig_name[idx]
+        return image_dir1
 
     def load_fig(self, idx):
         # print(idx)
-        image_dir1, image_dir2 = self.load_fig_dir(idx)
-        self.showTxt(2*idx)
-        print(image_dir1, image_dir2)
-        self.showImg(image_dir1, self.image1_x, self.image1_y)
-        self.showImg(image_dir2, self.image2_x, self.image2_y)
+        image_dir = self.load_fig_dir(idx)
+        self.showTxt(idx)
+        print(image_dir)
+        self.showImg(image_dir, self.image_x, self.image_y)
         self.idx += 1
 
 
 root = Tk()
-root.geometry("900x750")
+root.geometry("1000x700")
 app = Window(root)
 root.mainloop()
 
