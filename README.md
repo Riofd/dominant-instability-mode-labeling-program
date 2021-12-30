0b87d41422d4065fa6bec973c84b2e7933785704
Riofd <www.ahdic@qq.com>
2021-12-30 10:31:38 +0800

    bug paired

--- README.md
+++ README.md
@@ -10,8 +10,20 @@
 * 在菜单栏中“文件”下可以重新选择样本文件夹和标签文件夹
 * 本程序默认exe程序执行的是主导失稳模式样本的标注 
 ### **注：需要提前新建一个空的excel文件用于存放标签，并命名为‘__label.xlsx__’**
+## **3.Citation**
+[A graph attention networks-based model to distinguish the transient rotor angle instability and short-term voltage instability in power systems](https://www.sciencedirect.com/science/article/pii/S0142061521010036?via%3Dihub)
+```
+@article{Zhang2021DIM,
+  title={A Graph Attention Networks-Based Model to Distinguish the Transient Rotor Angle Instability and Short-term Voltage Instability in Power Systems},
+  author={R. Zhang, W. Yao, Z. Shi, et al.},
+  journal={International Journal of Electrical Power and Energy Systems},
+  year={2022}
+}
+```
 ## 2020/9/16更新
 * __修复了读取文档顺序的bug__
+## 2021/12/30更新
+* __修复了部分bug__
 ## __免责声明__
 * **本程序完全开源，作者不对程序错误等造成的后果负责**
-### <p align="right"> **Designed By RF from HUST SEEE**</p>
+### <p align="right"> **Designed By ZRF from HUST SEEE**</p>
--- label_exe.py
+++ label_exe.py
@@ -7,9 +7,6 @@ import time
 import xlwt
 import xlrd
 import openpyxl
-# image_dir1 = r'D:\Sample_Figure\PF100\1 AC line 100 50      3      9     10 2 0.1.matdelta.jpg'
-# image_dir2 = r'D:\Sample_Figure\PF100\1 AC line 100 50      3      9     10 2 0.1.matvol.jpg'
-# excel_dir = r'C:\Users\zhangrunfeng\Desktop\label.xlsx'
 
 
 class Window(Frame):
@@ -21,15 +18,15 @@ class Window(Frame):
         self.image_y = 100
         # self.image2_x = 0
         # self.image2_y = 400
-        self.fig_name = self.readFile()
+        self.fig_name = self.read_file()
         self.num_fig = len(self.fig_name) / 2
         self.init_window()
-        self.excel_dir = self.labelPath()
-        _, self.idx = self.getExcelinfo()
+        self.excel_dir = self.label_path()
+        _, self.idx = self.get_excel_info()
         self.load_fig(self.idx)
 
     def init_window(self):
-        self.master.title("主导失稳模式样本标注程序v2.0")
+        self.master.title("主导失稳模式样本标注程序v3.0")
 
         self.pack(fill=BOTH, expand=1)
 
@@ -39,39 +36,46 @@ class Window(Frame):
 
         # 创建File菜单，下面有Save和Exit两个子菜单
         file = Menu(menu)
-        file.add_command(label='样本文件夹', command=self.samplePath)
-        file.add_command(label='标签文件夹', command=self.labelPath)
+        file.add_command(label='样本文件夹', command=self.sample_path)
+        file.add_command(label='标签文件夹', command=self.label_path)
         file.add_command(label='退出程序', command=self.client_exit)
         menu.add_cascade(label='文件', menu=file)
 
         # 创建Edit菜单，下面有一个Undo菜单
         edit = Menu(menu)
         edit.add_command(label='Undo')
-        edit.add_command(label='Show  Image', command=self.showImg)
-        edit.add_command(label='Show  Text', command=self.showTxt)
+        edit.add_command(label='Show  Image', command=self.show_img)
+        edit.add_command(label='Show  Text', command=self.show_txt)
         menu.add_cascade(label='编辑', menu=edit)
-        ClickButton1 = Button(self, text='稳定', command=lambda: self.readExcel(0))
-        ClickButton1.place(x=840, y=200, width=100)
-        ClickButton2 = Button(self, text='功角失稳', command=lambda: self.readExcel(1))
-        ClickButton2.place(x=840, y=400, width=100)
-        ClickButton2 = Button(self, text='电压失稳', command=lambda: self.readExcel(2))
-        ClickButton2.place(x=840, y=600, width=100)
+        click_button1 = Button(self, text='稳定', command=lambda: self.write_to_excel(0))
+        click_button1.place(x=840, y=200, width=100)
+        click_button2 = Button(self, text='功角失稳', command=lambda: self.write_to_excel(1))
+        click_button2.place(x=840, y=400, width=100)
+        click_button3 = Button(self, text='电压失稳', command=lambda: self.write_to_excel(2))
+        click_button3.place(x=840, y=600, width=100)
+        # click_button4 = Button(self, text='电压失稳', command=lambda: self.read_excel(2))
 
-    def client_exit(self):
+    @staticmethod
+    def client_exit():
         exit()
 
-    def showImg(self, image_dir, image_x, image_y):
+    def show_img(self, image_dir, image_x, image_y):
         # load = Image.open(image_dir).resize((800, 600))
         load = Image.open(image_dir)
         width, height = load.size
-        load = load.resize((width//2, height//2))
+        load = load.resize((width//4, height//4))
+        box = (0, 100, 600, 980)  # 裁剪图片空白部分
+        load = load.crop(box)
         render = ImageTk.PhotoImage(load)
 
         img = Label(self, image=render)
         img.image = render
         img.place(x=image_x, y=image_y)
 
-    def showTxt(self, idx):
+    def show_txt(self, idx):
+        """
+        从文件名读取 PSASP 自动判稳结果
+        """
         figure_name = self.fig_name[idx]
         mode_id = figure_name[-5]
         if mode_id == '1':
@@ -80,26 +84,40 @@ class Window(Frame):
             info = '自动判稳模式：功角失稳'
         if mode_id == '3':
             info = '自动判稳模式：电压失稳'
+        if mode_id == '4':
+            info = '自动判稳模式：频率失稳'
         text = Label(self, text=info, font=('微软雅黑', 20, 'bold'))
         text.place(x=200, y=20)
 
-    def selectPath(self):
+    @staticmethod
+    def select_path():
+        """
+        路径选择函数，用以选择图片文件和标注文件
+        """
         path = askdirectory()
         # path.set(path)
         return path
 
-    def samplePath(self):
-
-        self.fig_name = self.readFile()
+    def sample_path(self):
+        """
+        初始化样本路径并加载图片
+        """
+        self.fig_name = self.read_file()
         self.idx = 0
         self.load_fig(self.idx)
 
-    def labelPath(self):
-        path = self.selectPath()
+    def label_path(self):
+        """
+        返回标签文件label.exe路径
+        """
+        path = self.select_path()
         excel_dir = path + '/label' + '.xlsx'
         return excel_dir
 
-    def getExcelinfo(self):
+    def get_excel_info(self):
+        """
+        读取excel表格中已经标注的样本位置
+        """
         excel_dir = self.excel_dir
         file = xlrd.open_workbook(excel_dir)
         table = file.sheets()[0]  # 得到sheet页
@@ -107,40 +125,60 @@ class Window(Frame):
         print(nrows)
         return excel_dir, nrows
 
-    def readExcel(self, label):
-        excel_dir, nrows = self.getExcelinfo()
+    def write_to_excel(self, label):
+        """
+        opeatation: 操作指令，default: 'write_label'，给当前图片写入标签
+        可选：'backward':读上一张图片
+        将标签自动写入excel文件，加载下一张图片
+        """
+        excel_dir, nrows = self.get_excel_info()
         wb = openpyxl.load_workbook(excel_dir)
         ws = wb['Sheet1']
-        ws.cell(row=nrows + 1, column=1).value = label
+        current_nrows = nrows + 1
+        ws.cell(row=current_nrows, column=1).value = label
         wb.save(excel_dir)
+        wb.close()
         self.load_fig(self.idx)
 
-    def search_all_files_return_by_time_reversed(self, path, reverse=False):
+    @staticmethod
+    def search_all_files_return_by_time_reversed(path, reverse=False):
+        """
+        按照时间排列顺序读取图片
+        """
         return sorted(glob.glob(os.path.join(path, '*')),
                       key=lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(os.path.getmtime(x))),
                       reverse=reverse)
 
-    def readFile(self):
-        path = self.selectPath()
+    def read_file(self):
+        """
+        读取图片文件名
+        """
+        path = self.select_path()
         fig_name = self.search_all_files_return_by_time_reversed(path)
         return fig_name
 
     def load_fig_dir(self, idx):
+        """
+        获取图片路径
+        """
         fig_name = self.fig_name
-        image_dir1 = fig_name[idx]
-        return image_dir1
+        image_dir = fig_name[idx]
+        return image_dir
 
     def load_fig(self, idx):
+        """
+        加载图片
+        """
         # print(idx)
         image_dir = self.load_fig_dir(idx)
-        self.showTxt(idx)
+        self.show_txt(idx)
         print(image_dir)
-        self.showImg(image_dir, self.image_x, self.image_y)
+        self.show_img(image_dir, self.image_x, self.image_y)
         self.idx += 1
 
 
 root = Tk()
-root.geometry("1000x700")
+root.geometry("1000x1000")
 app = Window(root)
 root.mainloop()
 
